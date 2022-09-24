83b4a847fa78ffcbe683b631b9161cd5eb36899d
anevski-stefan <anevskistefan84@gmail.com>
2022-09-24 13:46:17 +0200

    Fix contact form

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-let theme = localStorage.getItem('theme');
+let theme = localStorage.getItem("theme");
 
-if(theme == null){
-    setTheme('light');
+if (theme == null) {
+  setTheme("light");
 } else {
-    setTheme(theme);
+  setTheme(theme);
 }
 
 let themeDots = document.getElementsByClassName("theme-dot");
@@ -33,5 +33,42 @@ function setTheme(mode) {
     document.getElementById("theme-style").href = "purple.css";
   }
 
-  localStorage.setItem('theme', mode);
+  localStorage.setItem("theme", mode);
 }
+
+// Contact form
+
+var form = document.getElementById("contact-form");
+
+async function handleSubmit(event) {
+  event.preventDefault();
+  var status = document.getElementById("status");
+  var data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+      Accept: "application/json",
+    },
+  })
+    .then((response) => {
+      if (response.ok) {
+        status.innerHTML = "Thanks for your submission!";
+        form.reset();
+      } else {
+        response.json().then((data) => {
+          if (Object.hasOwn(data, "errors")) {
+            status.innerHTML = data["errors"]
+              .map((error) => error["message"])
+              .join(", ");
+          } else {
+            status.innerHTML = "Oops! There was a problem submitting your form";
+          }
+        });
+      }
+    })
+    .catch((error) => {
+      status.innerHTML = "Oops! There was a problem submitting your form";
+    });
+}
+form.addEventListener("submit", handleSubmit);
