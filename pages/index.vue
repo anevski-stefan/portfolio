037d183e4e1fd8386da063094adfcfcda2f37a78
Stefan Anevski <anevskistefan11@gmail.com>
2024-01-14 13:35:21 +0100

    Create Home Page

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<script setup>
+</script>
+
+<template>
+    <section>
+        <h1 class="text-5xl font-bold mt-20">👋Hello, I'm Stefan Anevski!</h1>
+        <p class="text-base text-gray-900 p-2 italic">Software Engineer at One inside</p>   
+    </section>
+    <section class="flex flex-col md:flex-row">
+        <div class="md:w-3/4"> 
+            <h2 class="text-3xl font-bold mt-8">My Story</h2>
+            <p class="text-lg py-2">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum aut culpa eius odio assumenda fugit autem, mollitia suscipit totam eaque, dolorum cum reiciendis natus maxime rem, modi hic? Quas, vel?
+            </p>
+            <p class="text-lg py-2">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum aut culpa eius odio assumenda fugit autem, mollitia suscipit totam eaque, dolorum cum reiciendis natus maxime rem, modi hic? Quas, vel?
+            </p>
+            <p class="text-lg py-2">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum aut culpa eius odio assumenda fugit autem, mollitia suscipit totam eaque, dolorum cum reiciendis natus maxime rem, modi hic? Quas, vel?
+            </p>
+        </div>
+        <img src="~/assets/images/me.jpg" alt="picture-of-me" class="w-1/2 md:max-w-sm p-8 mx-auto rounded-full">
+    </section>
+    <section>
+        <h2 class="text-3xl font-bold mt-8">Latest Blog Posts</h2>
+    </section>
+</template>
