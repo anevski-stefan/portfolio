09075f99713eb8714aeba06cf64cf7a10816f935
Stefan Anevski <anevskistefan11@gmail.com>
2024-01-25 22:14:52 +0100

    Add service component

---
+++ components/Service.vue
@@ -0,0 +1,13 @@
+<script setup>
+    const props = defineProps(['services'])
+</script>
+
+<template>
+    <div v-for="service in props.services" class="bg-white rounded-lg shadow-md overflow-hidden">
+        <div class="p-6">
+            <Icon :name="service.icon" size="3rem"/>
+            <h2 class="text-xl font-bold mb-2">{{ service.title }}</h2>
+            <p class="text-gray-700 mb-4">{{ service.description }}</p>
+        </div>
+    </div>
+</template>
